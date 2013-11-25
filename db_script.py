49d0bcb83a3b7f0795f94648542c423a2f26e95a
James Davis <jdavis1@dyn-128-59-157-69.dyn.columbia.edu>
2013-11-25 15:50:18 -0500

    working

--- db_script.py
+++ db_script.py
@@ -15,16 +15,24 @@ try:
     
     all_rows = []
     for row in rows:
-    	if row[1] == '':
-    		row_data = row['id'], 'no data'
-    	else:
-    		row_data = row['id'], row['date']
+        original_text = row['original_text']
+        original_text = original_text[:10000]
+        fulltext = row['fulltext']
+        fulltext = fulltext[:10000]
+
+    	row_data = (
+            row['id'], row['date'], original_text, row['scholarly_notes'],
+            row['manuscript_source'], row['printed_source'], row['authenticity'],
+            row['translation_notes'], row['keywords'], row['modified'], row['deleted'],
+            fulltext)
     	all_rows.append(row_data)
-    	#print row
 
     with open('test.csv', 'wb') as csvfile:
-		writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		writer.writerows(all_rows)
-		print all_rows
-except:
-	print "I can't select anything!"
+        writer = csv.writer(csvfile, delimiter='\t',quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerows(all_rows)
+		#print all_rows
+
+except Exception as e:
+    print '%s (%s)' % (e.message, type(e))
+    import pdb
+    pdb.set_trace()
