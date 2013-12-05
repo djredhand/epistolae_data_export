5079d150184da4e37a44d7d1a2c9c676602db3c4
James Davis <jdavis1@dyn-128-59-157-31.dyn.columbia.edu>
2013-12-05 13:08:22 -0500

    adding in mapper

--- participant.py
+++ participant.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
 import csv
+import pdb
 
 try:
 	conn = psycopg2.connect("dbname='epistolae' user='jdavis1' host='localhost' password='jedavis13'")
@@ -17,21 +18,28 @@ try:
     all_rows = []
     header = (['id','date','text','original_text','historical_context',
         'scholarly_notes', 'manuscript_source', 'printed_source','authenticity',
-        'translation_notes', 'keywords','modified','deleted','fulltext'])
+        'translation_notes', 'keywords','modified','deleted','fulltext', 'woman_name'])
     all_rows.append(header)
 
     for row in letter_rows:
-        text = row['text']
-        text = text[:10000]
-        original_text = row['original_text']
-        original_text = original_text[:10000]
-        historical_context = row['historical_context']
-        historical_context = historical_context[:10000]
-        fulltext = row['fulltext']
-        fulltext = fulltext[:10000]
+        letter_map_id = row['id']
+        cur.execute("SELECT * from participant WHERE letter_id = %s",(letter_map_id,))
+        letter_map = cur.fetchall()
+        for participant in letter_map:
+            participant_id = participant[0]
+            participant_letter_id = participant[1]
+            participant_woman_id = participant[2]
+            participant_name = participant[3]
+            participant_role = participant[4]
+
+            cur.execute("SELECT * from woman WHERE id = %s",(participant_woman_id,))
+            woman = cur.fetchall()
+            pdb.set_trace()
+        #letter_text = row['fulltext']
+        #letter_text = fulltext[:10000]
 
     	row_data = (
-            row['id'], row['date'], text, original_text, row['scholarly_notes'],
+            row['id'], row['date'], original_text, row['scholarly_notes'],
             row['manuscript_source'], row['printed_source'], row['authenticity'],
             row['translation_notes'], row['keywords'], row['modified'], row['deleted'],
             fulltext)
@@ -41,12 +49,11 @@ try:
             count = count + 1
 
 
-    with open('letter.csv', 'wb') as csvfile:
+    with open('participant.csv', 'wb') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerows(all_rows)
 		#print all_rows
 
 except Exception as e:
     print '%s (%s)' % (e.message, type(e))
-    import pdb
     pdb.set_trace()
